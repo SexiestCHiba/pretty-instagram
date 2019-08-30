9542bac7122af024d04edc7164555eede1bbafd7
Quentin Legot <quentin3157.github@gmail.com>
2019-08-30 18:05:22 +0200

    Update server.js
    + Added mateiral icons for multi photos and videos posts
    + Added no-store cache-control
    + Current search now appear in <input>
    * If a user haven't upload at least 1 posts, a message will now appear
    * Somes methods calls has been moved to prevent errors
    - deleted get /public/:fichier express access

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ var message ='<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8" /><titl
 '#nav .ig-title{position:absolute;margin-left: 20px;font-size:20px;font-size:1.5rem;}#nav .ig-search{position:absolute;left:50%;transform: translateX(-50%);width:max-content;height:48px;}#nav .ig-setting{position:absolute;right:0;margin-right: 20px;top:50%;transform: translateY(-50%);}'+
 '.material-icons{position: relative;top:7px;}'+
 '#main{margin:100px;}'+
-'.profile_pic{border-radius:50%;float:left;width:150px;height:150px;}.block_name{float:left;margin:0 20px;color: #262626;}'+ 
-'.photo{z-index:10;display:grid;grid-template-columns: 33% 33% 33%;grid-gap:0.4%;margin-top:30px;}.ig-post img{width:100%;min-height:50px;height:auto;}.ig-post{grid-column:span 1;grid-row:span 1;}' +
+'.profile_pic{border-radius:50%;float:left;width:150px;height:150px;}.block_name{float:left;margin:0 20px;color: #262626;}.material-icons.md-light{color:rgba(255, 255, 255, 1);}'+ 
+'.photo{z-index:10;display:grid;grid-template-columns: 33% 33% 33%;grid-gap:3px;margin-top:30px;}.ig-post img{width:100%;min-height:50px;height:auto;}.ig-post{grid-column:span 1;grid-row:span 1;}.graphIcon{position:absolute;width:max-content;z-index:15;}'+
 '@media screen and (max-width: 750px) {#main{margin:40px;}.ig-post img{min-height:30px;}}#search-icon{cursor:pointer;} '+
 '</style></head>'+
 '<body><div id="nav"><div class="ig-title">Pretty Instagram</div><div class="ig-search"><input id="input-search" type="text" placaholder="Search" value=""><i class="material-icons" id="search-icon">keyboard_arrow_right</i></div><div class="ig-setting"><i class="material-icons">more_vert</i></div></div>'+
+'<script type="text/javascript">document.getElementById(\'search-icon\').addEventListener("click", function(){window.location.href= document.getElementById(\'input-search\').value;});</script>'+
 '<div id="main">';
 
 
@@ -29,15 +30,18 @@ var loginInsta = async function (user, res) {
 	let profile;
 	let photo;
 	let milieuMessage = '';
+	let error = false;
 	try{
 	var client = new Instagram({username, password});
 	var loginUser = await client.login();
 	if(loginUser.status === 'ok'){
+		milieuMessage += '<script type="text/javascript">document.getElementById(\'input-search\').value="' + user + '";</script>';
 		profile = await client.getUserByUsername({username: user});
 		res.status(200);
 		milieuMessage += '<img class="profile_pic" src="' + profile.profile_pic_url + '"><span class="block_name"><h2>@' + profile.username +  '</h2><h3>'+ profile.full_name + '</h3><p class="biography">' + profile.biography + '</p></span><br style="clear:both;" />';
 		if(profile.is_private != true){
-
+			photo = await client.getPhotosByUsername({username: user}, 50, '');
+			milieuMessage = await displayPicture(profile, photo, milieuMessage);
 		}else{
 			milieuMessage += 'profil privé';
 		}
@@ -46,13 +50,13 @@ var loginInsta = async function (user, res) {
 	}
 }catch(err){
 	console.log('erreur 2');
-	console.log(err);
-	if(profile.statusCode === 404){
-		res.status(400).send(message + 'Utilisateur introuvable' + finMessage);
-		return;
+	error = true;
+	//console.log(err);
+	if(err.statusCode === 404){
+		milieuMessage += 'Utilisateur introuvable';
 	}
 	if(err.error.message){
-		milieuMessage = 'Instagram signale une erreur: ' + err.error.message;
+		milieuMessage += 'Instagram signale une erreur: ' + err.error.message;
 	}
 	if(err.error && err.error.message === 'checkpoint_required'){
 		const challengeUrl = err.error.checkpoint_url;
@@ -61,13 +65,8 @@ var loginInsta = async function (user, res) {
 		await client.updateChallenge({challengeUrl, securityCode: '301794'}); // <== securityCode - set code from email.
 	}
 }finally{
-	if(profile.is_private != true){
-		photo = await client.getPhotosByUsername({username: user}, 20, profile.edge_owner_to_timeline_media.edges[0].node.id);
-		milieuMessage = await displayPicture(profile, photo, milieuMessage);
-	}
-	milieuMessage += '<script type="text/javascript">document.getElementById(\'search-icon\').addEventListener("click", function(){window.location.href= document.getElementById(\'input-search\').value;});</script>';
+	if(error === true) milieuMessage += 'Une erreur est survenue';
 	res.send(message + milieuMessage + finMessage);
-	
 }
 }
 
@@ -107,15 +106,15 @@ var displayPicture = async function(profile, photo, milieuMessage){
 	let max = 50;
 	milieuMessage += '<div class="photo">';
 	if(profile.edge_owner_to_timeline_media.count < max) max = profile.edge_owner_to_timeline_media.count;
+	if(max === 0 ) milieuMessage += 'Aucune image a affiché';
 	for(let x = 0; x < max; x++){
-		let  graphVideo = false;
 		milieuMessage = milieuMessage + '<div class="ig-post">';
-		//if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename == 'GraphSidecar'){milieuMessage += '<i class="material-icons" style="z-index:5;">filter</i>'; graphVideo=true;} else{ if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename === 'GraphVideo'){graphVideo=true; milieuMessage+= '<i class="material-icons" style="z-index:5;">';}}
-		if(x<6){ milieuMessage += '<img src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"'; milieuMessage += '>'; }else{milieuMessage += '<img class="lazy" data-src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"';/* if(graphVideo==true){milieuMessage+= 'style="position:relative;top:-24px;"'} */milieuMessage+= '>';}
+		if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename == 'GraphSidecar'){milieuMessage += '<div class="graphIcon"><i class="material-icons md-light">filter</i></div>';} else{ if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename === 'GraphVideo'){milieuMessage+= '<div class="graphIcon"><i class="material-icons md-light">videocam</i></div>';}}
+		if(x<6){ milieuMessage += '<a href="#"><img src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"'; milieuMessage += '>'; }else{milieuMessage += '<img class="lazy" data-src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"';milieuMessage+= '>';}
 
 		milieuMessage += '</div>';
 	}
-	milieuMessage += '<script type="text/javascript">/* document.getElementById(\'lazyload\').getBoundingClientRect.; */ document.addEventListener("DOMContentLoaded", function() {var lazyloadImages = document.querySelectorAll("img.lazy");var lazyloadThrottleTimeout;lazyload();'+
+	milieuMessage += '<script type="text/javascript">document.addEventListener("DOMContentLoaded", function() {var lazyloadImages = document.querySelectorAll("img.lazy");var lazyloadThrottleTimeout;lazyload();'+
 	'function lazyload () {if(lazyloadThrottleTimeout) {clearTimeout(lazyloadThrottleTimeout);} '+  
 	'lazyloadThrottleTimeout = setTimeout(function() {var scrollTop = window.pageYOffset;lazyloadImages.forEach(function(img) {if(img.offsetTop < (window.innerHeight + scrollTop)) {img.src = img.dataset.src;img.classList.remove(\'lazy\');}});'+
 	'if(lazyloadImages.length == 0) { document.removeEventListener("scroll", lazyload);window.removeEventListener("resize", lazyload);window.removeEventListener("orientationChange", lazyload);}'+
@@ -129,15 +128,13 @@ var displayPicture = async function(profile, photo, milieuMessage){
 app.use(express.static(__dirname + '/public'))
 .use(favicon(__dirname + '/public/logo.png'))
 .get('/', function(req, res){
- 	res.status(200);
+	res.setHeader('Cache-Control', 'no-store, no-cache, public, no-transform');
  	let user = "instagram";
 	loginInsta(user, res);
 	 
 })
-.get('/public/:fichier', function(req, res){
-
-})
 .get('/:nick', function(req, res){
+	res.setHeader('Cache-Control', 'no-store, no-cache, public, no-transform');
 	loginInsta(req.params.nick, res);
 })
 .use(function(req, res, next){
