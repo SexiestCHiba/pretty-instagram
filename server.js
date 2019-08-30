213d5c6b806d692ebf632b9667902986839c5deb
Quentin Legot <quentin3157.github@gmail.com>
2019-08-30 13:32:46 +0200

    + Added a blank logo.png * fixed display issues

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const Instagram = require('instagram-web-api');
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
-var message ='<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8" /><title>Pretty Instagram</title><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+
+var message ='<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8" /><title>Pretty Instagram</title><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>'+
 '<style>html{background-color: #fafafa;font-family:Verdana, Geneva, Tahoma, sans-serif;;}'+
 'body{margin:0;border:0;padding:0;font-size:16px;font-size:1rem;height:100%;}'+
-'#nav{position:fixed;top:0;left:0;width:100%;height:48px;line-height: 48px;background-color: white;box-shadow: 0px 2px 1px rgba(0, 0, 0, 0.1);font-family: \'Courier New\', Courier, monospace;}'+
+'#nav{z-index:100;position:fixed;top:0;left:0;width:100%;height:48px;line-height: 48px;background-color: white;box-shadow: 0px 2px 1px rgba(0, 0, 0, 0.1);font-family: \'Courier New\', Courier, monospace;}'+
 '#nav .ig-title{position:absolute;margin-left: 20px;font-size:20px;font-size:1.5rem;}#nav .ig-search{position:absolute;left:50%;transform: translateX(-50%);width:max-content;height:48px;}#nav .ig-setting{position:absolute;right:0;margin-right: 20px;top:50%;transform: translateY(-50%);}'+
 '.material-icons{position: relative;top:7px;}'+
 '#main{margin:100px;}'+
 '.profile_pic{border-radius:50%;float:left;width:150px;height:150px;}.block_name{float:left;margin:0 20px;color: #262626;}'+ 
-'.photo{display:grid;grid-template-columns: 33% 33% 33%;grid-gap:0.4%;margin-top:30px;}.ig-post img{width:100%;min-height:50px;height:auto;}.ig-post{grid-column:span 1;grid-row:span 1;}' +
+'.photo{z-index:10;display:grid;grid-template-columns: 33% 33% 33%;grid-gap:0.4%;margin-top:30px;}.ig-post img{width:100%;min-height:50px;height:auto;}.ig-post{grid-column:span 1;grid-row:span 1;}' +
 '@media screen and (max-width: 750px) {#main{margin:40px;}.ig-post img{min-height:30px;}}#search-icon{cursor:pointer;} '+
 '</style></head>'+
 '<body><div id="nav"><div class="ig-title">Pretty Instagram</div><div class="ig-search"><input id="input-search" type="text" placaholder="Search" value=""><i class="material-icons" id="search-icon">keyboard_arrow_right</i></div><div class="ig-setting"><i class="material-icons">more_vert</i></div></div>'+
@@ -108,7 +108,12 @@ var displayPicture = async function(profile, photo, milieuMessage){
 	milieuMessage += '<div class="photo">';
 	if(profile.edge_owner_to_timeline_media.count < max) max = profile.edge_owner_to_timeline_media.count;
 	for(let x = 0; x < max; x++){
-		milieuMessage = milieuMessage + '<div class="ig-post">'; if(x<6){ milieuMessage += '<img src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"></div>'; }else{milieuMessage += '<img class="lazy" data-src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"></div>';} 
+		let  graphVideo = false;
+		milieuMessage = milieuMessage + '<div class="ig-post">';
+		//if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename == 'GraphSidecar'){milieuMessage += '<i class="material-icons" style="z-index:5;">filter</i>'; graphVideo=true;} else{ if(photo.user.edge_owner_to_timeline_media.edges[x].node.__typename === 'GraphVideo'){graphVideo=true; milieuMessage+= '<i class="material-icons" style="z-index:5;">';}}
+		if(x<6){ milieuMessage += '<img src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"'; milieuMessage += '>'; }else{milieuMessage += '<img class="lazy" data-src="' + photo.user.edge_owner_to_timeline_media.edges[x].node.thumbnail_src + '"';/* if(graphVideo==true){milieuMessage+= 'style="position:relative;top:-24px;"'} */milieuMessage+= '>';}
+
+		milieuMessage += '</div>';
 	}
 	milieuMessage += '<script type="text/javascript">/* document.getElementById(\'lazyload\').getBoundingClientRect.; */ document.addEventListener("DOMContentLoaded", function() {var lazyloadImages = document.querySelectorAll("img.lazy");var lazyloadThrottleTimeout;lazyload();'+
 	'function lazyload () {if(lazyloadThrottleTimeout) {clearTimeout(lazyloadThrottleTimeout);} '+  
